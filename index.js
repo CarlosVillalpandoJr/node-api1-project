af5dd6a668228c75594d45a8d3168163b49836e6
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-02 16:21:39 -0600

    set up server to run on port 4001

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+
+const db = require('./data/db');
+const express = require('express');
+const server = express();
+
+server.use(express.json()); // needed to parse JSON from the body
+
+server.get('/', (req, res) => {
+    res.send({ api: 'up and running, yea!...' })
+})
+
+const port = 4001;
+server.listen(port, () => console.log(`!!API RUNNING ON PORT ${port} OH YEAH!!`))
