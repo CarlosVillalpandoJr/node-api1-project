08e06329118d48c4744f3917a75ff850bdfc3a29
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-02 16:32:10 -0600

    GET request for users functioning

--- index.js
+++ index.js
@@ -10,5 +10,19 @@ server.get('/', (req, res) => {
     res.send({ api: 'up and running, yea!...' })
 })
 
+// GET request that returns array of all user objects: /api/users
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+            console.log('Error in the GET /api/users')
+            res
+                .status(500)
+                .json( {errorMessage: 'The users information could not be retrieved.'} )
+        })
+})
+
 const port = 4001;
 server.listen(port, () => console.log(`!!API RUNNING ON PORT ${port} OH YEAH!!`))
