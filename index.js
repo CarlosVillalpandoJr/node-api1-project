5eed810b09ba375db3a407e79423d2701d6a61e7
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-02 18:45:28 -0600

    added get by id request

--- index.js
+++ index.js
@@ -24,5 +24,40 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+        .then(id => {
+            if(id) {
+                res.status(200).json(id)
+            } else {
+                res.status(404)
+                .json( {message:'The user with the specified ID does not exist'})
+            }
+        })
+        .catch(error => {
+            console.log('Error in the GET /api/users/:id')
+            res
+                .status(500)
+                .json( {error: 'The user information could not be retrieved.'} )
+        })
+})
+
+
+
+// POST request that creates user using the info sent in req body
+server.post('/api/users', (req, res) => {
+    const userData = req.body;
+    if(userData.name && userData.bio) {
+        db.insert(userData)
+        .then(user => {
+            res.status(201).json({...user, ...userData})
+        })
+    } else {
+        res.status(400).json({ errorMessage: 'Provide name and bio for user' })
+    }
+})
+
+
 const port = 4001;
 server.listen(port, () => console.log(`!!API RUNNING ON PORT ${port} OH YEAH!!`))
